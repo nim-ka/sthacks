b709ff3ba3d00995c949fdbc5af8e2360a0827cb
nim-ka <raumaank1@gmail.com>
2020-02-05 23:37:02 -0500

    rotate

--- xander-prep/geo.inc.c
+++ xander-prep/geo.inc.c
@@ -5,7 +5,7 @@ const GeoLayout xander_geo[] = {
     GEO_OPEN_NODE(),
         GEO_SCALE(0x00, 16384),
         GEO_OPEN_NODE(),
-            GEO_DISPLAY_LIST(LAYER_OPAQUE, xander_dl_opaque),
+            GEO_ROTATE_WITH_DL(LAYER_OPAQUE, 0x4000, 0x0000, 0x4000, xander_dl_opaque),
         GEO_CLOSE_NODE(),
     GEO_CLOSE_NODE(),
     GEO_END()
--- xander/geo.s
+++ xander/geo.s
@@ -5,10 +5,10 @@ glabel xander_geo
 	.word	486539264
 	.word	16384
 	.word	67108864
-	.word	352387072
+	.word	279003136
+	.word	16384
 	.word	xander_dl_opaque
 	.word	83886080
 	.word	83886080
 	.word	16777216
-
 glabel xander_geo_end
